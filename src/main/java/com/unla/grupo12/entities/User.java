e8cd230b4e3db980de9136398478b34e11a8da62
Rodrigo Espindola <rodrispd@hotmail.com>
2021-05-03 03:29:47 -0300

    Agregamos un model y entity User para testear que la conexion con la base de datos es correcta

---
+++ src/main/java/com/unla/grupo12/entities/User.java
@@ -0,0 +1,42 @@
+package com.unla.grupo12.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="apellido")
+	private String apellido;
+	
+	@Column(name="tipoDocumento")
+	private String tipoDocumento;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	
+	
+	
+	
+	
+
+}
