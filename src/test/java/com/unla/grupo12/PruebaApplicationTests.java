8e8d2c404340425db05da728d4ba4e4ddac43db5
Rodrigo Espindola <rodrispd@hotmail.com>
2021-05-19 23:11:28 -0300

    Se agrega la dependencia Lombok al pom.xml

---
+++ src/test/java/com/unla/grupo12/PruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
